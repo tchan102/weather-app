c2d74e66c280b4805b7525ce8d80dc36ceb0d8ec
Tat Chung Chan <tatchungchan@Tats-MacBook-Air.local>
2023-12-08 13:11:04 -0800

    add api key

--- src/weatherService.js
+++ src/weatherService.js
@@ -1,4 +1,4 @@
-const API_KEY = 'YOURAPIKEY';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const makeIconURL = (iconId) => `https://openweathermap.org/img/wn/${iconId}@2x.png`
 const getFormattedWeatherData = async (city, units = 'metric') => {
